164e70f7e917ffcb63f6eda4076f6c1bb4b46170
valursp <vsp@hi.is>
2014-03-14 22:19:56 +0000

    Tri Peaks klasi update

--- TriPeaks.py
+++ TriPeaks.py
@@ -1,18 +1,31 @@
 from Deck import *
 from Card import *
+import time
 
 class TriPeaks(object):
 
     #Smidur
     def __init__(self):
-        deck = Deck()
-        board = Deck()
-        heap = Deck()
+        #Stokkur leiksins
+        self.deck = Deck(52)
+        self.deck.setCards()
+        self.deck.shuffleCards()
+        
+        #Spil sem eru i bordi
+        self.board = Deck(0)
+        #Spil sem eru i hrugu
+        self.heap = Deck(0)
     
-    #Segir hvort adgerd er logleg
-    def isLegal(self):
-        raise Exception("Fall ekki tilbuid")
-
+        #Breyta sem heldur utanum stig
+        self.score = 0
+    
+        #Breyta sem byrjar ad taka tima
+        self.start_time = time.time()
+    
+    #Segir hvort adgerd er logleg, skilar true ef satt annars false
+    def isLegal(self, card):
+        return self.heap.getNextCard(len(self.heap)) == card
+    
     #Skrifar ut stodu spila a bordi
     def boardShow(self):
         raise Exception("Fall ekki tilbuid")
@@ -23,20 +36,26 @@ class TriPeaks(object):
 
     #Faerir spil ur bordi i hrugu
     def toHeap(self):
-        raise Exception("Fall ekki tilbuid")
+        self.heap.append(self.deck.pop())
 
     #Dregur spil ur stokk
     def drawCard(self):
-        raise Exception("Fall ekki tilbuid")
+        self.deck.pop()
 
     #Heldur utanum stig leiksins
-    def score(self):
-        raise Exception("Fall ekki tilbuid")
+    def score(self, points):
+        self.score += points
 
     #Tekur tima leiksins
     def measureTime(self):
-        raise Exception("Fall ekki tilbuid")
+        return time.time() - self.start_time
 
-    #Athugar hvort leikur se buinn, t.e.a.s. er buid ad vinna eda tapa
+    #Athugar hvort leikur se buinn, t.e.a.s. er buid ad vinna
     def hasWon(self):
-        raise Exception("Fall ekki tilbuid")
+        return (self.deck != 0 and self.board == 0)
+
+def main():
+    newGame = TriPeaks()
+
+if __name__ == "__main__":
+    sys.exit(main())
